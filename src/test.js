8cc6541ae98236553032fd7e2865b6d05dba117b
Mung Nguyen <mungyp98@gmail.com>
2019-04-26 23:41:50 +0700

    test

--- src/test.js
+++ src/test.js
@@ -4,3 +4,5 @@ console.log("duoc nghi roi, that thoai mai qua di <# ^^ :D");
 console.log("abcbcbbbc thầy mà nhìn thấy hai con nghịch git kiểu này chắc chết");
 
 console.log("Thầy sẽ phát hiện ra sinh viên của thầy cục kì cực kì sáng tạo, kkaka");
+
+console.log("Mong là sẽ có cách");
