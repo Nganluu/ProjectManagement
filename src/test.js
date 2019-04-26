6c959c642263111a2af6656a37c15815320271e3
Nganluu <nganltt712@gmail.com>
2019-04-26 23:47:17 +0700

    abc

--- src/test.js
+++ src/test.js
@@ -4,3 +4,7 @@ console.log("duoc nghi roi, that thoai mai qua di <# ^^ :D");
 console.log("abcbcbbbc thầy mà nhìn thấy hai con nghịch git kiểu này chắc chết");
 
 console.log("Thầy sẽ phát hiện ra sinh viên của thầy cục kì cực kì sáng tạo, kkaka");
+console.log("abcbcbbbc thầy mà nhìn thấy hai con nghịch git kiểu này chắc chết")
+console.log("test phát nữa")
+
+console.log("day la luc chua pull cai gi ve ca nhe")
